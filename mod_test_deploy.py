7095008e716c266da310e3f9864dff50ff3ceb68
tazgal <mailboxtasos@gmail.com>
2025-09-28 22:48:28 +0300

    Update mod_test_deploy.py

--- mod_test_deploy.py
+++ mod_test_deploy.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write("Αυτή είναι μια δοκιμαστική εφαρμογή")
 
-text = st.text_area("Enter text","text",height = 600)
+text = st.text_area("Enter text",height = 200)
 
 if st.button("send to text"):
     st.write(f"{text}")
