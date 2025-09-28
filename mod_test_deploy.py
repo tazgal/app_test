bda72aa4e09d8f507c2590558f16bab1500419b5
tazgal <mailboxtasos@gmail.com>
2025-09-28 22:50:46 +0300

    Update mod_test_deploy.py

--- mod_test_deploy.py
+++ mod_test_deploy.py
@@ -6,3 +6,6 @@ text = st.text_area("Enter text",height = 200)
 
 if st.button("send to text"):
     st.write(f"{text}")
+
+if st.button("open Ριζοσπάστη"):
+    webbrowser.open_new_tab("https://www.rizospastis.gr")
