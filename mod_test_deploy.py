0b53cbfe633de35ad54fa93451e4e1cb9996f5d8
tazgal <mailboxtasos@gmail.com>
2025-09-29 22:56:41 +0300

    Update mod_test_deploy.py

--- mod_test_deploy.py
+++ mod_test_deploy.py
@@ -2,9 +2,43 @@ import streamlit as st
 import re
 from nltk.tokenize import sent_tokenize
 import nltk
+import streamlit as st
+from youtube_transcript_api import YouTubeTranscriptApi
+from urllib.parse import urlparse, parse_qs
+import streamlit as st
+import whisper
+import tempfile
 
 # Κατέβασε το punkt μόνο αν χρειάζεται
 nltk.download("punkt", quiet=True)
+
+
+
+
+def transcribe_youtube_streamlit():
+    # Παίρνουμε URL από τον χρήστη
+    url = st.text_input("Please enter YouTube URL: ")
+
+    # Ανάλυση URL για να πάρουμε το video_id
+    parsed_url = urlparse(url)
+    video_id = parse_qs(parsed_url.query).get('v')
+    if video_id:
+        video_id = video_id[0]  # παίρνουμε το πρώτο αποτέλεσμα
+    else:
+        raise ValueError("Δεν βρέθηκε video_id στο URL")
+
+    # Κλήση στο API
+    api = YouTubeTranscriptApi()
+    transcript = api.fetch(video_id, languages=['el','en'])
+
+    # Προσπέλαση με attributes
+    full_text = "\n".join([item.text for item in transcript])
+    st.write(full_text)
+
+
+if st.button("Απομαγνητοφώνηση από youtube"):
+        transcribe_youtube_streamlit()
+
 
 def extract_date_sentences(text):
     """Εμφανίζει προτάσεις που περιέχουν ημερομηνίες."""
